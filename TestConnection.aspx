dd9227b44f6d4f10ace60577fbc6c81e3c7991e5
Ibaan <ibaanibrahim123098@gmail.com>
2026-02-13 09:25:49 +0800

    Add database connection

---
+++ TestConnection.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestConnection.aspx.cs" Inherits="LearnSphere_WAPP.TestConnection" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin:50px">
+            <h2>Database Connection Test</h2>
+
+            <asp:Button ID="btnTest" runat="server" 
+                Text="Test Connection" 
+                OnClick="btnTest_Click" />
+
+            <br /><br />
+
+            <asp:Label ID="lblResult" runat="server" 
+                Font-Bold="true" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ TestConnection.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LearnSphere_WAPP
+{
+	public partial class TestConnection : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnTest_Click(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["LearnSphereDB"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    lblResult.Text = "Database Connected Successfully!";
+                    lblResult.ForeColor = System.Drawing.Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    lblResult.Text = "Connection Failed: " + ex.Message;
+                    lblResult.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+    }
+}
