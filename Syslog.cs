34dcec6f730e6dafececcff67cdbbc26c2e6db99
FOO QI CHANG <TP080526@mail.apu.edu.my>
2026-03-02 14:25:32 +0800

    Admin page & syslog class

---
+++ Syslog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LearnSphere_WAPP
+{
+    public class Syslog
+    {
+        static public void action(int UserID, string action)
+        {
+            DateTime curr = DateTime.Now;
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["LearnSphereDB"].ConnectionString);
+            con.Open();
+
+            string query = "insert into Syslog (userid, action, dateTime) values (@UserID, @Action, @DateTime)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@UserID", UserID);
+            cmd.Parameters.AddWithValue("@Action", action);
+            cmd.Parameters.AddWithValue("@DateTime", curr);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        } 
+    }
+}
